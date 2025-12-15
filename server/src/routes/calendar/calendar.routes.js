eb4b08fd4a24e4218c8915f61f38b5199482e37f
Isidro Noé Salvador Jiménez <isidronoe.sj@gmail.com>
2025-12-15 13:40:45 -0600

    Eliminar carpeta server

--- server/src/routes/calendar/calendar.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const calendarController = require("../../controllers/calendar/calendar.Controller");
-const verifyToken = require("../../middleware/authMiddleware");
-
-router.get("/getCalendar", verifyToken, calendarController.getCalendar);
-router.put("/changeStatus", verifyToken, calendarController.changeStatus);
-
-module.exports = router;
--- server/src/routes/talones/talones.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const talonesController = require("../../controllers/talones/talones.Controller");
-const verifyToken = require("../../middleware/authMiddleware");
-
-router.get("/perfil-talon/:id", verifyToken, talonesController.getProfile);
-router.get("/talones-pendientes-entregar", verifyToken, talonesController.getAllTalonesPendintesEntregar);
-router.get("/talones-pendientes-regresar", verifyToken, talonesController.getAllTalonesPendientesRegresar);
-module.exports = router;
